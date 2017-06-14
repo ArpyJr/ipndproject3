76d932cb392d62b8eac87b165dfdbaff81f0190b
Jooyong Chung <jychung0801@gmail.com>
2017-06-13 20:36:48 -0500

    init, code have greeting, choose difficulty prompt, and strings for difficulties

---
+++ quiz.py
@@ -0,0 +1,28 @@
+print 'Please choose a difficulty "easy" / "medium" / "hard"'
+#user chooses difficulty, stores in variable difficulty and prints back the difficulty
+#as a response
+difficulty = raw_input('choose difficulty ')
+print 'You chose ' + difficulty + '!'
+
+
+#string for quiz -----easy-----
+easy_paragraph = """Butterflies are amazing creatures. Their life cycles has four stages.
+They begin life as an egg. The egg hatches and a caterpillar emerges.
+The caterpillar eats for several days until it is ready to make a coccoon.
+The caterpillar gestates inside the cocoon and eventually re-emerges as a beautiful butterlfy.
+The butterfly will go on to feed and pollinate flowers."""
+
+#string for quiz -----medium-----
+medium_paragraph = """The beach is a favourite summer spot for many, and there are plenty of options for what to do.
+If you're in tropical waters, you may enjoy snorkeling, as almost any level swimmer can take part.
+You'll be able to see lots of fish and enjoy the clear waters.
+If you're feeling brave, you might try guided scooba diving.
+Explore the depths and see hidden treasures and wild sea life not accesible at the surface."""
+
+#string for quiz -----hard-----
+hard_paragraph = """David Bowie is a music icon who has influenced generations of musicians.
+He was born David Roberts Jones in 1948, and quickly went on to become an influential figure in the music world.
+Bowie's first major hallmark was his album 'The Rise and Fall of Ziggy Stardust and the Spiders From Mars.'
+From that point, Bowie continued to reinvent himself and redefine the genre through the ages.
+Bowie was awarded five posthumous awards for his latest album 'Blackstar' after his tragic passing in 2016. """
+
